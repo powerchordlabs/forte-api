6133e375ca0d93ae311e13c88142947f8a40ee56
Michael Van Diest <mvandiest@gmail.com>
2016-01-14 12:00:44 -0500

    update tests
    PDVI-2399

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ var assert = require('chai').assert
 describe('forteApi', function(){
 	describe('constructor', function(){
 		it('should verify that credentials have been provided')
+		it('should verify that a trunk scope has been provided')
 		it('should warn if fingerPrintingEnabled is true on the server')
 	})
 	describe('api.on', function(){
@@ -11,8 +12,20 @@ describe('forteApi', function(){
 		it('should invoke the callback function on auth error')
 	})
 	describe('api.log', function(){
-		it('should throw an error if an invalid log level is used')
-		it('should throw an error if an invalid log message is used')
+		it('should throw an error if log level is invalid')
+		it('should throw an error if message is invalid')
 		it('should throw an error if meta is supplied, but it is not an object')
+	})
+	describe('api.organizations', function(){
+		describe('.getMany', function(){
+			it('should throw an erorr if filter is null')
+			describe('when a request succeeds, the return value', function(){
+				it('should have a "response" property')
+				it('should have a "result" property')
+			})
+		})
+		describe('.getOne', function(){
+			it('should ')
+		})
 	})
 })
