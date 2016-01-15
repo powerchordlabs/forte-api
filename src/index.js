1fd8f3d94923677b808860b045ffab2de7650fec
Michael Van Diest <mvandiest@gmail.com>
2016-01-15 14:52:53 -0500

    update tests
    PDVI-2399

--- src/index.js
+++ src/index.js
@@ -1,5 +1,66 @@
-module.exports = function(){
-	return function(){
-		
+var impl = require('implementjs')
+
+exports = module.exports = createAPI
+
+/* istanbul ignore next */
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+function createAPI(credentials, scope, options) {
+	verfifyCredentials(credentials)
+	verfifyScope(scope)
+
+	return forteApi(credentials, scope, options)
+}
+
+function forteApi(credentials, scope, options) {
+	return {
+		withBranch: function(id) {
+			if(id === undefined){
+				throw impl.NotImplementedError
+			}
+			
+			var newScope = _extends({}, scope)
+			newScope.branch = id
+			return createAPI(credentials, newScope, options)
+		}
 	}
+}
+
+var credentialBearerImpl = {
+	bearerToken: impl.S
+}
+var credentialSharedKeyImpl = {
+	privateKey: impl.S,
+	publicKey: impl.S,
+}
+
+function verfifyCredentials(credentials) {
+	if(!credentials){
+		throw impl.NotImplementedError
+	}
+	if(credentials.bearerToken !== undefined) {
+		impl.implements(credentials, credentialBearerImpl)
+		return
+	}
+	impl.implements(credentials, credentialSharedKeyImpl)
+}
+
+var trunkScopeImpl = {
+	trunk: impl.S
+}
+
+var branchScopeImpl = {
+	trunk: impl.S,
+	branch: impl.S
+}
+
+function verfifyScope(scope) {
+	if(!scope){
+		throw new impl.NotImplementedError
+	}
+	if(scope.branch !== undefined) {
+		impl.implements(scope, branchScopeImpl)
+		return
+	}
+	impl.implements(scope, trunkScopeImpl)
 }
--- test/index.js
+++ test/index.js
@@ -1,31 +1,97 @@
 var assert = require('chai').assert
+var forteApi = require('../src')
+var NotImplementedError = require('implementjs').NotImplementedError
 
 describe('forteApi', function(){
+	function apiFactory(){
+		var args = arguments
+		return function() { 
+			return forteApi.apply(null, args)
+		}
+	}
+
+	var validTokenCreds = {bearerToken: 'valid'}
+	var validKeyCreds = {privateKey: 'valid', publicKey: 'valid'}
+	var validTrunkScope = { trunk: 'valid' }
+
+	var invalidBranchScopes = [null, 1, {}, function(){}]
+
 	describe('constructor', function(){
-		it('should verify that credentials have been provided')
-		it('should verify that a trunk scope has been provided')
-		it('should warn if fingerPrintingEnabled is true on the server')
+		it('should throw if invalid credentials have been provided', function(){
+			var invalidCredentials = [
+				undefined,
+				null,
+				{},
+				{bearerToken: null},
+				{bearerToken: 0},
+				{privateKey: null, publicKey: null},
+				{privateKey: 0, publicKey: 0}
+			]
+			invalidCredentials.forEach(function(invalidCreds){
+				assert.throws(apiFactory(invalidCreds, validTrunkScope))
+			})
+		})
+
+		it('should NOT throw if valid credentials have been provided', function(){
+			assert.doesNotThrow(apiFactory(validTokenCreds, validTrunkScope))
+			assert.doesNotThrow(apiFactory(validKeyCreds, validTrunkScope))
+		})
+
+		it('should throw if an invalid a trunk scope has been provided', function(){
+			assert.throws(apiFactory(validTokenCreds, {}))
+		})
+
+		it('should throw if an invalid a branch scope has been provided', function(){
+			invalidBranchScopes.forEach(function(scope){
+				assert.throws(apiFactory(validTokenCreds, {trunk:'valid', branch: scope}))
+			})
+		})
+		it('should throw if any options are not properly typed')
 	})
+
+	describe('api.withBranch', function(){
+		var api
+		beforeEach(function(){
+			api = apiFactory(validTokenCreds, validTrunkScope)()
+		})
+
+		it('should throw if an invalid a branch scope has been provided', function(){
+			invalidBranchScopes.concat(undefined).forEach(function(scope){
+				assert.throws(function() { api.withBranch(scope) })
+			})
+		})
+
+		it('should throw an error if ID is not a non-empty string')
+		it('should return a new instance that has branch scope')
+	})
+
 	describe('api.on', function(){
-		it('should throw an error if callback is not a function')
+		it('should throw if callback is not a function')
 		it('should invoke the callback function on auth success')
 		it('should invoke the callback function on auth error')
 	})
+
 	describe('api.log', function(){
-		it('should throw an error if log level is invalid')
-		it('should throw an error if message is invalid')
-		it('should throw an error if meta is supplied, but it is not an object')
+		it('should throw if log level is invalid')
+		it('should throw if message is invalid')
+		it('should throw if meta is supplied, but is not an object')
 	})
+
 	describe('api.organizations', function(){
 		describe('.getMany', function(){
 			it('should throw an erorr if filter is null')
 			describe('when a request succeeds, the return value', function(){
 				it('should have a "response" property')
+				it('should have a "response.data" property')
+			})
+			describe('when a request fails, the return value', function(){
+				it('should have a "response" property')
 				it('should have a "result" property')
 			})
 		})
 		describe('.getOne', function(){
-			it('should ')
+			it('should throw if filter is null')
+
 		})
 	})
 })
