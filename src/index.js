e2c3d10e0c23ca357e746e09e800d51820153644
Michael Van Diest <mvandiest@gmail.com>
2016-01-18 11:00:06 -0500

    reorganized arg validators
    PDVI-2399

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ var InvalidArgumentError = util.InvalidArgumentError
 exports = module.exports = createApi
 
 function createApi(credentials, scope, options) {
-	verifyCreateApiArgs.apply(null, arguments)
+	validateArgs('createApi', arguments)
 	return forteApi.apply(null, arguments)
 }
 
 function forteApi(credentials, scope, options) {
 	return {
 		withBranch: function(id) {
-			verifyWithBranchArgs.apply(null, arguments)
+			validateArgs('withBranch', arguments)
 
 			var newScope = assign({}, scope, { branch: id})
 			return createApi(credentials, newScope, options)
@@ -21,109 +21,112 @@ function forteApi(credentials, scope, options) {
 			return scope
 		},
 		on: function(name, callback) {
-			verifyOnArgs.apply(null, arguments)
+			validateArgs('on', arguments)
 		},
 		log: function(level, message, meta) {
-			verifyLogArgs.apply(null, arguments)
+			validateArgs('log', arguments)
 		}
 
 	}
 }
 
+/* 
+ * method validations
+ */
 var LOG_LEVELS = ['trace', 'debug', 'info', 'warn', 'error', 'fatal'];
 
-/* 
- * Verifcations
- */
 function argumentError(name) {
 	throw new InvalidArgumentError(name)
 }
 
-function verifyCreateApiArgs(credentials, scope, options) {
-	verifyCredentials(credentials)
-	verifyScope(scope)
-	verifyOptions(options)
+function validateArgs(method, arguments) {
+	validators[method].apply(null, arguments)
 }
 
-function verifyCredentials(credentials) {
-	if(typeof credentials === 'undefined'){
-		argumentError('credentials')
-	}
+var validators = {
+	createApi: function createApi(credentials, scope, options) {
+		function verifyCredentials(credentials) {
+			if(typeof credentials === 'undefined'){
+				argumentError('credentials')
+			}
 
-	if(credentials.bearerToken !== undefined) {
-		if(typeof credentials.bearerToken !== 'string'){
-			argumentError('credentials.bearerToken')
+			if(credentials.bearerToken !== undefined) {
+				if(typeof credentials.bearerToken !== 'string'){
+					argumentError('credentials.bearerToken')
+				}
+				return
+			}
+
+			if(typeof credentials.privateKey !== 'string'){
+				argumentError('credentials.privateKey')
+			}
+
+			if(typeof credentials.publicKey !== 'string'){
+				argumentError('credentials.publicKey')
+			}
 		}
-		return
-	}
 
-	if(typeof credentials.privateKey !== 'string'){
-		argumentError('credentials.privateKey')
-	}
+		function verifyScope(scope) {
+			if(!scope){
+				throw new InvalidArgumentError('scope')
+			}
+			
+			if(typeof scope.trunk !== 'string' || scope.trunk === ''){
+				argumentError('scope.trunk')
+			}
 
-	if(typeof credentials.publicKey !== 'string'){
-		argumentError('credentials.publicKey')
-	}
-}
+			if(scope.branch !== undefined && typeof scope.branch !== 'string' || scope.branch === ''){
+				argumentError('scope.branch')
+			}
+		}
 
-function verifyScope(scope) {
-	if(!scope){
-		throw new InvalidArgumentError('scope')
-	}
-	
-	if(typeof scope.trunk !== 'string' || scope.trunk === ''){
-		argumentError('scope.trunk')
-	}
+		function verifyOptions(options) {
+			if(options === undefined) {
+				return
+			}
 
-	if(scope.branch !== undefined && typeof scope.branch !== 'string' || scope.branch === ''){
-		argumentError('scope.branch')
-	}
-}
+			if(options.url === undefined && options.fingerPrintingEnabled === undefined) {
+				argumentError('options')
+			}
 
-function verifyOptions(options) {
-	if(options === undefined) {
-		return
-	}
+			if(options.url !== undefined && typeof options.url !== 'string') {
+				argumentError('options.url')
+			}
 
-	if(options.url === undefined && options.fingerPrintingEnabled === undefined) {
-		argumentError('options')
-	}
+			if(options.fingerPrintingEnabled !== undefined && typeof options.fingerPrintingEnabled !== 'boolean') {
+				argumentError('options.fingerPrintingEnabled')
+			}
+		}
 
-	if(options.url !== undefined && typeof options.url !== 'string') {
-		argumentError('options.url')
-	}
+		verifyCredentials(credentials)
+		verifyScope(scope)
+		verifyOptions(options)
+	},
+	withBranch: function withBranch(id) {
+		if(id === undefined){
+			throw new InvalidArgumentError('id')
+		}
+	},
+	log: function log(level, message, meta){
+		if(LOG_LEVELS.indexOf(level) === -1) {
+			argumentError('Log level "' + level + '" is invalid. Use one of: ' + LOG_LEVELS.join(', '))
+		}
 
-	if(options.fingerPrintingEnabled !== undefined && typeof options.fingerPrintingEnabled !== 'boolean') {
-		argumentError('options.fingerPrintingEnabled')
-	}
-}
+		if(typeof message !== 'string' || message.trim() === '') {
+			argumentError('Message "' + message + '" is invalid.')
+		}
 
-function verifyWithBranchArgs(id) {
-	if(id === undefined){
-		throw new InvalidArgumentError('id')
-	}
-}
+		if(meta !== undefined && (meta === null || typeof meta !== 'object')){
+			argumentError('Meta "' + meta + '" is invalid.')
+		}
+	},
+	on: function on(name, callback) {
+		if(name !== 'auth') {
+			argumentError('"' + name + '" is not a supported event.')
+		}
 
-function verifyLogArgs(level, message, meta) {
-	if(LOG_LEVELS.indexOf(level) === -1) {
-		argumentError('Log level "' + level + '" is invalid. Use one of: ' + LOG_LEVELS.join(', '))
-	}
-
-	if(typeof message !== 'string' || message.trim() === '') {
-		argumentError('Message "' + message + '" is invalid.')
-	}
-
-	if(meta !== undefined && (meta === null || typeof meta !== 'object')){
-		argumentError('Meta "' + meta + '" is invalid.')
-	}
-}
-
-function verifyOnArgs(name, callback) {
-	if(name !== 'auth') {
-		argumentError('"' + name + '" is not a supported event.')
-	}
-
-	if(typeof callback !== 'function') {
-		argumentError('callback must be a function.')
+		if(typeof callback !== 'function') {
+			argumentError('callback must be a function.')
+		}
 	}
 }
