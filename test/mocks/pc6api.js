9e380a9a3ad412a95fc22294307beca9c66bf8e2
Michael Van Diest <mvandiest@gmail.com>
2016-05-24 00:35:44 -0400

    refactor: format code, add patch and delete

--- test/mocks/pc6api.js
+++ test/mocks/pc6api.js
@@ -3,38 +3,50 @@ import nock from 'nock'
 export const MOCK_AUTH_TOKEN = 'FAKE_AUTH_TOKEN_FROM_NOCK'
 
 const defaultOptions = {
-	//allowUnmocked: true,
+//allowUnmocked: true,
 }
 
 export default {
-	get: (url, status = 200, response = '') => {
-		return nock('http://api.pclocal.us', defaultOptions)
-			.matchHeader('Authorization', matchRequestAuthHeader)
-	    	.get(url)
-	    	.reply(status, response, okHeaders)
-	},
-	post: (url, status = 200, body) => {
-		return nock('http://api.pclocal.us', defaultOptions)
-			.matchHeader('Authorization', matchRequestAuthHeader)
-	    	.post(url, body)
-	    	.reply(status, formatNockResponse, okHeaders)
-	},
-	put: (url, status = 200, body) => {
-		return nock('http://api.pclocal.us', defaultOptions)
-			.matchHeader('Authorization', matchRequestAuthHeader)
-	    	.put(url, body)
-	    	.reply(status, formatNockResponse, okHeaders)
-	}
+  get: (url, status = 200, response = '') => {
+    return nock('http://api.pclocal.us', defaultOptions)
+    .matchHeader('Authorization', matchRequestAuthHeader)
+    .get(url)
+    .reply(status, response, okHeaders)
+  },
+  post: (url, status = 200, body) => {
+    return nock('http://api.pclocal.us', defaultOptions)
+    .matchHeader('Authorization', matchRequestAuthHeader)
+    .post(url, body)
+    .reply(status, formatNockResponse, okHeaders)
+  },
+  put: (url, status = 200, body) => {
+    return nock('http://api.pclocal.us', defaultOptions)
+    .matchHeader('Authorization', matchRequestAuthHeader)
+    .put(url, body)
+    .reply(status, formatNockResponse, okHeaders)
+  },
+  patch: (url, status = 200, body) => {
+    return nock('http://api.pclocal.us', defaultOptions)
+    .matchHeader('Authorization', matchRequestAuthHeader)
+    .patch(url, body)
+    .reply(status, formatNockResponse, okHeaders)
+  },
+  delete: (url, status = 200, response = '') => {
+    return nock('http://api.pclocal.us', defaultOptions)
+    .matchHeader('Authorization', matchRequestAuthHeader)
+    .delete(url)
+    .reply(status, response, okHeaders)
+  }
 }
 
 function formatNockResponse(uri, body) {
-	return JSON.parse(body)
+  return JSON.parse(body)
 }
 
 const okHeaders = {
-	'Authorization': MOCK_AUTH_TOKEN,
+  'Authorization': MOCK_AUTH_TOKEN
 }
 
 function matchRequestAuthHeader(value) {
-	return value === 'Bearer VALID_TOKEN' || /^Checksum VALID_PUBLICKEY:(\d+):([^:]+):(dealer\.client\.us)$/.test(value)
+  return value === 'Bearer VALID_TOKEN' || /^Checksum VALID_PUBLICKEY:(\d+):([^:]+):(dealer\.client\.us)$/.test(value)
 }
