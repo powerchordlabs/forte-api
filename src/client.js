e186eb9509eaecd6d11044f637b7aac2189b7d46
Anthony Atkinson <sha1sum@users.noreply.github.com>
2016-12-13 13:35:01 -0500

    feat: User-Based Authentication Capabilities (#9)
    * Documentation updated for accuracy.
    
    * Documentation updated for accuracy.
    
    * Support for user authentication with email/password credentials.

--- src/client.js
+++ src/client.js
@@ -1,5 +1,6 @@
 import crypto from 'crypto'
 import superagent from 'superagent'
+import InvalidArgumentError from './util'
 const debug = require('debug')('forte-api:client')
 
 const METHODS = ['get', 'post', 'put', 'patch', 'delete']
@@ -10,7 +11,7 @@ function authMiddleware(superagent, hostname, credentials) {
 
     if (credentials.bearerToken) {
       authHeader = `${credentials.bearerToken}`
-    } else {
+    } else if (credentials.privateKey) {
       const UTCTimestamp = Math.floor((new Date()).getTime() / 1000)
       const FQDN = hostname
       const checkSumData = [credentials.privateKey, credentials.publicKey, UTCTimestamp, FQDN].join(':')
@@ -20,6 +21,10 @@ function authMiddleware(superagent, hostname, credentials) {
       debug('hash %s', hash)
 
       authHeader = `Checksum ${[credentials.publicKey, UTCTimestamp, hash, FQDN].join(':')}`
+    } else if (credentials.email) {
+      return this
+    } else {
+      throw new InvalidArgumentError('No publicKey/privateKey or email/password credentials were provided.')
     }
 
     this.set('Authorization', authHeader)
@@ -59,8 +64,8 @@ class Client {
         request.end((err, res) => {
           if(err) { debug(`client.${method} error: %o`, err) }
 
-          // all succesful api responses have auth header <- NOT TRUE on token auth
-          // all succesful CHECKSUM api responses have auth header, keep the bearerToken if not passed
+          // all successful api responses have auth header <- NOT TRUE on token auth
+          // all successful CHECKSUM api responses have auth header, keep the bearerToken if not passed
           onAuth && onAuth(err, err ? null : (res.headers.authorization ? res.headers.authorization : credentials.bearerToken))
 
           err ? reject(err) : resolve(res)
